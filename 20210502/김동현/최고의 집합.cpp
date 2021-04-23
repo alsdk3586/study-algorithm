15191ac7956210da9a5b5501d43602166c21895a
mild2987 <62752377+mild2987@users.noreply.github.com>
2021-04-24 08:58:42 +0900

    Create 최고의 집합.cpp

---
+++ 20210502/김동현/최고의 집합.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    int quot = s / n;
+    int rem = s % n;
+    
+    if (quot <= 0)
+        return vector<int>{ -1 };
+    
+    vector<int> ret(n, quot);
+    if (rem == 0)
+        return ret;
+    else
+    {
+        for (auto it = ret.rbegin(); rem > 0; it++, rem--)
+            (*it)++;
+        
+        return ret;
+    }
+}
