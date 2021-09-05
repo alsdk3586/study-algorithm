cfb502080d4f0c4e50f1499949363d05595c265d
dimsssss <caruen@hanmail.net>
2021-09-05 10:34:05 +0900

    refactor: 리뷰 내용 반영
    Description
    불필요한 if else 삭제

--- 9월/20210905/박다정/p_3.py
+++ 9월/20210905/박다정/p_3.py
@@ -6,17 +6,10 @@ n, m = map(int, input().rstrip().split(' '))
 numbers = list(map(int, input().rstrip().split(' ')))
 ranges = [list(map(int, input().rstrip().split(' '))) for _ in range(m)]
 table = [0 for _ in range(n + 1)]
-accumulate = 0
 
 for i in range(len(numbers)):
     table[i + 1] = table[i] + numbers[i]
 
 for i in range(len(ranges)):
     start, end = ranges[i]
-
-    if start == end:
-        print(numbers[start - 1])
-    elif start == 1:
-        print(table[end])
-    else:
-        print(table[end] - table[start - 1])
+    print(table[end] - table[start - 1])
