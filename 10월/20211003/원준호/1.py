9ab51e9ad309eafda4a5528f4944c3aeca1a33bd
shshsh123 <55295680+junho-one@users.noreply.github.com>
2021-10-02 22:39:49 +0900

    Update 1.py

--- 10월/20211003/원준호/1.py
+++ 10월/20211003/원준호/1.py
@@ -1,23 +1,17 @@
 from collections import deque, defaultdict
 
 def solution(priorities, location):
-    
-    papers = defaultdict(int)
-    for p in priorities:
-        papers[p] += 1
-    
-    priorities = deque([(idx, val) for idx, val in enumerate(priorities)])
-    best = max(priorities, key = lambda x : x[1])
-    print(priorities, best)
-    answer = []
-    while priorities:    
-        best = max(priorities, key = lambda x : x[1])
-        now = priorities.popleft()
-        
-        if now[1] == best[1]:
-            answer.append(now[0])
-    
-        else :
-            priorities.append(now)
-    
-    return answer.index(location)+1
+    N = len(priorities)
+    priorities = [(idx, val) for idx, val in enumerate(priorities)]
+    answer = {}
+    order = 1
+    while order <= N:
+        max_pr = max(priorities, key=lambda x : x[1])
+        pr = priorities.pop(0)
+        if pr[1] == max_pr[1]:
+            answer[pr[0]] = order
+            order += 1
+        else:
+            priorities.append(pr)
+
+    return answer[location]
