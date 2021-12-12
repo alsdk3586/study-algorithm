7df6362d29804b1f4ffdbe0f8ea232f2974a6846
kimCoding <xowns4817@naver.com>
2021-12-12 23:37:46 +0900

    nqueen 최적화 코드

--- 12월/20211212/김태준/nqueen.java
+++ 12월/20211212/김태준/nqueen.java
@@ -1,73 +1,89 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
- 
-	// n by n 체크판에 n개의 퀸을 놀려면 각 줄에 1개씩 놔야함
-	static int n;
-	static int chessBoard[ ][ ];
-	static int res = 0;
-	
-	
-	public static void main(String args[ ]) {
-		
-		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt();
-	
-		chessBoard = new int[n][n];
-		for(int i=0; i<n; i++) {
-			Arrays.fill(chessBoard[i], 0);
-		}
-	
-		dfs(0);
-		System.out.println(res);
-	};
-	
-	static void dfs(int d) {
-		
-		if(d == n) {
-			res++;
-			return;
-		}
-		
-		for(int i=0; i<n; i++) {
-			if(chessBoard[d][i] == 0) {
-				fillChessBoard(d, i, 1);
-				dfs(d + 1);
-				fillChessBoard(d, i, -1);
-			}
-		}		
-	}
-	
-	static void fillChessBoard(int xp, int yp, int v) {
-		
-		//세로
-		for(int i=0; i<n; i++) {
-			if(i==xp) continue;
-			chessBoard[i][yp] += v;	
-		}
- 		
-		// 대각선
-		int xp2 = xp + 1;
-		int yp2 = yp + 1;
-		//우하
-		while(xp2 < n && yp2 < n) {
-			chessBoard[xp2][yp2] += v;
-			xp2 += 1;
-			yp2 += 1;
-		}
-		
-		xp2 = xp + 1;
-		yp2 = yp - 1;
-		// 좌하
-		while(xp2 < n && 0 <= yp2) {
-			chessBoard[xp2][yp2] += v;
-			xp2 += 1;
-			yp2 -= 1;
-		}
-	}
+
+    // n by n 체크판에 n개의 퀸을 놀려면 각 줄에 1개씩 놔야함
+    static int n;
+    static int chessBoard[ ][ ];
+    static List<Integer> colResList = new ArrayList<>();
+
+    public static void main(String args[ ]) {
+
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt ();
+
+        chessBoard = new int[n][n];
+        for(int i=0; i<n; i++) {
+            Arrays.fill(chessBoard[i], 0);
+        }
+
+        int res = dfs(0);
+        if(n%2==0) System.out.println(res*2);
+        else {
+            int size = colResList.size();
+            if(size < 2) System.out.println(res);
+            else {
+                int colA = colResList.get(size - 1);
+                int colB = colResList.get(size - 2);
+                System.out.println(colB * 2 + (colA - colB));
+            }
+        }
+    };
+
+    static int dfs(int d) {
+
+        if(d == n) return 1;
+
+        int ret = 0;
+        int idx = n;
+        if(d == 0) {
+            if(n%2 == 0) idx = (n/2);
+            else idx = (n/2) + 1;
+        }
+
+        for(int i=0; i<idx; i++) {
+            if(chessBoard[d][i] == 0) {
+                fillChessBoard(d, i, 1);
+                ret += dfs(d + 1);
+                if(d==0) {
+                    colResList.add(ret);
+                }
+                fillChessBoard(d, i, -1);
+            }
+        }
+
+        return ret;
+    }
+
+    static void fillChessBoard(int xp, int yp, int v) {
+
+        //세로
+        for(int i=0; i<n; i++) {
+            if(i==xp) continue;
+            chessBoard[i][yp] += v;
+        }
+
+        // 대각선
+        int xp2 = xp + 1;
+        int yp2 = yp + 1;
+        //우하
+        while(xp2 < n && yp2 < n) {
+            chessBoard[xp2][yp2] += v;
+            xp2 += 1;
+            yp2 += 1;
+        }
+
+        xp2 = xp + 1;
+        yp2 = yp - 1;
+        // 좌하
+        while(xp2 < n && 0 <= yp2) {
+            chessBoard[xp2][yp2] += v;
+            xp2 += 1;
+            yp2 -= 1;
+        }
+    }
 }
 
