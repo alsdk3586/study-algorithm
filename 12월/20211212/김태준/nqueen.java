ef73f7d7b7fa8baa41f09f14c43fbca12f003608
kimCoding <xowns4817@naver.com>
2021-12-12 23:46:58 +0900

    nqueen 설명추가

--- 12월/20211212/김태준/nqueen.java
+++ 12월/20211212/김태준/nqueen.java
@@ -3,6 +3,19 @@ import java.util.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+ * nqueen 최적화 코드 풀이
+ * - nqueen의 퀸 배치를 보면 답이되는 case가 쌍을 이루는 특징을 가지고 있다. 따라서, 1행의 n/2까지만 dfs를 탐색해서 결과에 *2를하면 
+ *   1행의 탐색을 n -> n/2가 되므로 탐색시간이 줄게된다.
+ *
+ *   여기서 주의할 점은 n이 홀수/짝수를 구분해줘야한다.
+ *
+ *   짝수 : (n/2)까지 돌고 결과에 *2
+ *   홀수 : 1열기준 각 행에 퀸을 놓았을때 문제의 조건을 만족하는 경우의수를 colResList로 관리한다.
+ *         (n/2)+1 까지 돌고 colResList의 마지막전 요소*2 + (colResList의 마지막원소 - colResList의 마지막 전 요소)
+ *         
+ *   
+ */
 public class Main {
 
     // n by n 체크판에 n개의 퀸을 놀려면 각 줄에 1개씩 놔야함
